09ed48e692b3c9b68afa607dfe014b32b9c93d4e
soywiz <soywiz@gmail.com>
2024-07-24 09:30:39 +0200

    Document annotations

--- korlibs-annotations/src/korlibs/annotations/DeprecatedParameter.kt
+++ korlibs-annotations/src/korlibs/annotations/DeprecatedParameter.kt
@@ -1,5 +1,8 @@
 package korlibs.annotations
 
+/**
+ * Marks a parameter as deprecated.
+ */
 @Target(AnnotationTarget.VALUE_PARAMETER)
 annotation class DeprecatedParameter(
     val reason: String
